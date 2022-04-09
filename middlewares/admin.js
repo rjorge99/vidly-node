b2b25328f34991433098d37e61e7cdb0dd068987
Jorge Reyes <rjorge99@hotmail.com>
2022-04-09 13:44:01 -0500

    Se agrega prueba unitaria para generacion de token del ususuario

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,7 +1,6 @@
 module.exports = function (req, res, next) {
     // 401 Unauthorized
     // 403 Forbidden
-    console.log(req.user);
     if (!req.user.isAdmin) return res.status(403).send('Acciess denied, not permissions');
 
     next();
